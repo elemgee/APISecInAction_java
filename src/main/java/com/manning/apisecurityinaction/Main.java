f878e21d6d0f83743c792cb72f50ce1056ec823e
Larry Mills-Gahl <elemgee@gmail.com>
2019-06-12 14:01:43 -0400

    initial base setup

---
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -0,0 +1,10 @@
+package com.manning.apisecurityinaction;
+
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "API Security in Action\n(using Akka-HTTP for Java instead of Spray.io)" );
+    }
+}
